e01a49d9ed99db5b816722ac309e70f53d4c24f2
Takaaki Umada <taka.umada@gmail.com>
2018-03-15 13:17:43 +0900

    search

---
+++ dojo/search.js
@@ -0,0 +1,77 @@
+/* global instantsearch */
+
+var search = instantsearch({
+  appId: '2V364IP9ZG',
+  apiKey: '073d76b35b445cc665f3a5083bc3e91b',
+  indexName: 'entredojo2018',
+  urlSync: {}
+});
+
+search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: '#q'
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.stats({
+    container: '#stats'
+  })
+);
+
+var hitTemplate =
+  '<div class="hit media">' +
+    '<div class="media-body">' +
+      '<h4 class="media-heading"><a href="{{{_highlightResult.startup_url.value}}} ">{{{_highlightResult.startup_name.value}}}</a></h4>' +
+      '<p>{{{_highlightResult.oneliner.value}}}</p>' +
+    '</div>' +
+  '</div>';
+
+var noResultsTemplate =
+  '<div class="text-center">No results found matching <strong>{{query}}</strong>.</div>';
+
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#hits',
+    hitsPerPage: 10,
+    templates: {
+      empty: noResultsTemplate,
+      item: hitTemplate
+    },
+    transformData: function(hit) {
+      hit.stars = [];
+      for (var i = 1; i <= 5; ++i) {
+        hit.stars.push(i <= hit.rating);
+      }
+      return hit;
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#pagination',
+    cssClasses: {
+      root: 'pagination',
+      active: 'active'
+    }
+  })
+);
+
+search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: '#genres',
+    attributeName: 'genre',
+    operator: 'and',
+    limit: 10,
+    cssClasses: {
+      list: 'nav nav-list',
+      count: 'badge pull-right',
+      active: 'active'
+    }
+  })
+);
+
+
+
+search.start();
